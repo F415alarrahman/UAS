f773956f8a7c8317a1f7ddf78d0fa8c13f4e364a
F415alarrahman <111266988+F415alarrahman@users.noreply.github.com>
2023-01-17 13:03:58 +0700

    UAS
    UAS

---
+++ uas.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class uas {
+    public static void main(String[] args) {
+        ArrayList<Integer> B = new ArrayList<>();
+        B.add(20);
+        B.add(30);
+        B.add(40);
+        System.out.println(B.get(0));
+        System.out.println(B.get(1));
+        System.out.println(B.get(2));
+    }
+}
